15a150c9a815777f7c09b5b156f1126bd9312990
unknown <jacob.jasperson@yahoo.com>
2023-02-26 22:34:50 -0700

    added packages and fixed auth

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+//add apollo and middleware
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+//add typDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer(
+  {
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  }
+)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
+
+// app.use(routes);
+
+
+//start apollo server
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
